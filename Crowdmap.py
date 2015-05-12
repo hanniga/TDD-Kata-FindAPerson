125c3cad4e96df791048e09e4eff53643d3297f3
Reuven Yagel <robi@post.jce.ac.il>
2015-05-12 09:40:52 +0300

    REFACTOR: improve param name

--- Crowdmap.py
+++ Crowdmap.py
@@ -2,5 +2,5 @@ class Crowdmap(object):
     def __init__(self, init_list):
       self.list = init_list
       
-    def get_all_posts_for(self, param):
-        return [post for post in self.list if post.find(param) != -1]
+    def get_all_posts_for(self, name):
+        return [post for post in self.list if post.find(name) != -1]
