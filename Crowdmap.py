5f0e4dc64379eb42368b7f1f852f52a1e59f13eb
Reuven Yagel <robi@post.jce.ac.il>
2015-05-19 08:28:11 +0300

    REFACTOR: rename private members

--- Crowdmap.py
+++ Crowdmap.py
@@ -1,15 +1,15 @@
 class Crowdmap(object):
     def __init__(self, init_list):
-      self.list = init_list
-      self.location_service = LocationService()
+      self._list = init_list
+      self._location_service = LocationService()
       
     def get_all_posts_for(self, name):
-        return [post for post in self.list if post.find(name) != -1]
+        return [post for post in self._list if post.find(name) != -1]
 
     def is_location_for_name(self, name):
         name_posts = self.get_all_posts_for(name)
         for post in name_posts:
-            if self.location_service.find(post):
+            if self._location_service.find(post):
                 return True;
         return False
 
