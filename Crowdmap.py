c16b05a8fe401ca29f4c95e727789465a380b785
Reuven Yagel <robi@post.jce.ac.il>
2015-05-19 08:26:11 +0300

    GREEN: using stub location service

--- Crowdmap.py
+++ Crowdmap.py
@@ -1,9 +1,18 @@
 class Crowdmap(object):
     def __init__(self, init_list):
       self.list = init_list
+      self.location_service = LocationService()
       
     def get_all_posts_for(self, name):
         return [post for post in self.list if post.find(name) != -1]
 
     def is_location_for_name(self, name):
-        return True
+        name_posts = self.get_all_posts_for(name)
+        for post in name_posts:
+            if self.location_service.find(post):
+                return True;
+        return False
+
+class LocationService:
+    def find(self, text):
+        return (text.find("Bangkok") != -1)
