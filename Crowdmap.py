89b5040101580f0a039a39599b5834d5d3fe0916
hanniga <hlamash@gmail.com>
2015-05-28 12:18:17 +0300

    RED-test_missinfLocationInformationReturnsFalse

--- Crowdmap.py
+++ Crowdmap.py
@@ -1,17 +1,21 @@
 class Crowdmap(object):
     def __init__(self, init_list):
-      self._list = init_list
+      self.list = init_list
       self._location_service = LocationService()
-      
+    
     def get_all_posts_for(self, name):
-        return [post for post in self._list if post.find(name) != -1]
+        return [post for post in self.list if post.find(name) != -1]
 
     def is_location_for_name(self, name):
-        name_posts = self.get_all_posts_for(name)
-        for post in name_posts:
-            if self._location_service.find(post):
-                return True;
-        return False
+		posts = [post for post in self.list if post.find(name) != -1 and post.find("at") != -1]
+		return len(posts) != 0
+
+    def if_there_are_map_inconsistencies(self, name):
+		posts = [post for post in self.list if post.find(name) != -1 and post.find("at") != -1]
+		x=posts[0].index("at")
+		posts1 = [post for post in self.list if post.find(name) != -1 and post.find(posts[0][x:x+4]) != -1]
+		print '\n'+posts[0][x:x+4]
+		return len(posts) != len(posts1)
 
 class LocationService:
     def find(self, text):
